d8526378a99dfecb885a751d1f90fafaca5acb4e
FlavorfulGecko5 <FlavorfulGecko5#0596>
2023-03-27 21:52:06 -0700

    PVP Entities files

--- Build/BuildMods.bat
+++ Build/BuildMods.bat
@@ -10,10 +10,10 @@ if exist %OUTPUT_CLASSIC_AC% (del %OUTPUT_CLASSIC_AC%)
 if exist %OUTPUT_ETERNAL% (del %OUTPUT_ETERNAL%)
 if exist %OUTPUT_ETERNAL_AC% (del %OUTPUT_ETERNAL_AC%)
 
-EternalModBuilder.exe -c ..\Configs\_Common.json -c ..\Configs\Classic_Common.json -c ..\Configs\Classic.json -s ..\GameFiles -o Classic.zip -t 4
-EternalModBuilder.exe -c ..\Configs\_Common.json -c ..\Configs\Classic_Common.json -c ..\Configs\ClassicAC.json -s ..\GameFiles -o ClassicAC.zip -t 4
-EternalModBuilder.exe -c ..\Configs\_Common.json -c ..\Configs\Eternal_Common.json -c ..\Configs\Eternal.json -s ..\GameFiles -o Eternal.zip -t 4
-EternalModBuilder.exe -c ..\Configs\_Common.json -c ..\Configs\Eternal_Common.json -c ..\Configs\EternalAC.json -s ..\GameFiles -o EternalAC.zip -t 4
+EternalModBuilder.exe -c ..\Configs\_Common.json -c ..\Configs\Classic_Common.json -c ..\Configs\Classic.json -s ..\GameFiles -o Classic.zip -t 8
+EternalModBuilder.exe -c ..\Configs\_Common.json -c ..\Configs\Classic_Common.json -c ..\Configs\ClassicAC.json -s ..\GameFiles -o ClassicAC.zip -t 8
+EternalModBuilder.exe -c ..\Configs\_Common.json -c ..\Configs\Eternal_Common.json -c ..\Configs\Eternal.json -s ..\GameFiles -o Eternal.zip -t 8
+EternalModBuilder.exe -c ..\Configs\_Common.json -c ..\Configs\Eternal_Common.json -c ..\Configs\EternalAC.json -s ..\GameFiles -o EternalAC.zip -t 8
 
 echo Build process complete for all mod versions
 pause
